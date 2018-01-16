fbb3a8b1919d8f27086b0da36f1cf4386f32d649
Tae Wook Ha -X (taeha - BIS CONSULTING INC at Cisco) <taeha@cisco.com>
2018-01-16 10:04:47 -0800

    testing my first r package

---
+++ util/R/cat_function.R
@@ -0,0 +1,13 @@
+#' this is a documentation
+#' blahblah
+
+
+
+cat_function <- function(love=TRUE){
+  if(love==TRUE){
+    print("I love cats!")
+  }
+  else {
+    print("I am not a cool person.")
+  }
+}
